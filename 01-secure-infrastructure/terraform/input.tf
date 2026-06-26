d14de9a0423a2106dd09a5590dbda7919b44bf8e
Abdulkareem Mutolib <mutolibabdulkareem@gmail.com>
2026-06-26 11:01:01 +0100

    Update input.tf

--- 01-secure-infrastructure/terraform/input.tf
+++ 01-secure-infrastructure/terraform/input.tf
@@ -1,11 +1,11 @@
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "Deployment-resources"
+resource "azurerm_resource_group" "Secure-Lab-RG" {
+  name     = "Secure-Lab"
   location = "West Europe"
 }
 
 #
-resource "azurerm_subnet_network_security_group_association" "example" {
-  subnet_id                 = azurerm_subnet.example.id
-  network_security_group_id = azurerm_network_security_group.example.id
+resource "azurerm_subnet_network_security_group_association" "bastion_subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.bastion_subnet.id
+  network_security_group_id = azurerm_network_security_group.NSG1.id
 }
